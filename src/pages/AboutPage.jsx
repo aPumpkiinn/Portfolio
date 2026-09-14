ce2d641bc8e980af692127c9d04740a8759f0c68
aPumpkiinn <156137186+aPumpkiinn@users.noreply.github.com>
2026-09-14 11:10:20 +0200

    Fix spacing in AboutPage.jsx text content

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -78,7 +78,7 @@ const AboutPage = ({ onOpenContact }) => {
                         </p>
 
                         <p className="text-lg md:text-xl text-white font-sans leading-relaxed max-w-lg font-medium border-l-4 border-[#00c8ff] pl-4">
-                            Dans cette optique, je suis actuellement à la recherche d'une <span className="text-[#00c8ff] font-bold">alternance</span> de <span className="text-[#00c8ff] font-bold">1 an</span> à partir de<span className="text-[#00c8ff] font-bold">Septembre</span>. 
+                            Dans cette optique, je suis actuellement à la recherche d'une <span className="text-[#00c8ff] font-bold">alternance</span> de <span className="text-[#00c8ff] font-bold">1 an</span> à partir de <span className="text-[#00c8ff] font-bold">Septembre</span>. 
                             Je souhaite rejoindre une équipe passionnée où je pourrai apporter ma rigueur et ma créativité, 
                             tout en continuant à apprendre aux côtés de professionnels.
                         </p>
